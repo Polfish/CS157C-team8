de714f28309be0859c3102fc2bcd0ae9c9ca87e9
Paul Chon <45519040+Polfish@users.noreply.github.com>
2022-05-24 18:07:01 +0000

    Fixing Liked Songs page to have interactive like/dislike

--- recmu/web-react/src/components/LikedSongs.js
+++ recmu/web-react/src/components/LikedSongs.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import { useQuery, gql } from '@apollo/client'
-// import { useEffect } from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
-// import Badge from 'react-bootstrap/Badge'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import Button from 'react-bootstrap/Button'
-
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-// import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import LikeButton from './LikeButton'
 
 const GET_LIKED_SONGS_QUERY = gql`
   query($userName: String) {
@@ -15,18 +9,7 @@ const GET_LIKED_SONGS_QUERY = gql`
   }
 `
 
-// const REMOVE_SONG_FROM_LIKES_QUERY = gql`
-//   query($userName: String, $songName: String) {
-//     removeLikeFromSong(userName: $userName, songName: $songName)
-//   }
-// `
-
-// const REMOVE_SONG_FROM_LIKES_QUERY = gql``
-
 export default function LikedSongs() {
-  // const [likedSongs, setLikedSongs] = useState([])
-  // const [userName, setUsername] = useState('Default')
-  // const [songToDislike, setSongToDislike] = useState('')
   const userName = 'Test'
   const { loading, error, data } = useQuery(GET_LIKED_SONGS_QUERY, {
     variables: { userName },
@@ -37,53 +20,21 @@ export default function LikedSongs() {
     return <p>Error: help!</p>
   }
   console.log('Liked songs: ' + data.findLikedSongs)
-  // const [dislikeSongQuery, { loading2, error2, data2 }] = useLazyQuery(
-  //   REMOVE_SONG_FROM_LIKES_QUERY
-  // )
-  // const [dislikeSongQuery, { l, e, d }] = useLazyQuery(
-  //   REMOVE_SONG_FROM_LIKES_QUERY
-  // )
-  // useEffect(() => {
-  //   if (songToDislike) {
-  //     dislikeSongQuery({
-  //       variables: {
-  //         // userName: userName,
-  //         songName: songToDislike,
-  //       },
-  //     })
-  //   }
-  // }, [])
-  // const dislikeSong = (e) => {
-  //   const songName = e.target.name
-  //   setSongToDislike(songName)
-  //     // dislikeSongQuery({
-  //     //   variables: { userName, songName },
-  //     // })
-  // }
-  const createLikedSongsList = (row) => {
+  const createLikedSongsList = (result) => {
     return (
       <ListGroup.Item
         as="li"
         className="d-flex justify-content-between align-items-start"
       >
         <div className="ms-2 me-auto">
-          <div className="fw-bold">{row}</div>
+          <div className="fw-bold">{result}</div>
         </div>
-        <FontAwesomeIcon
-          className="mx-4"
-          style={{ color: 'red', fontSize: '20px' }}
-          icon={faHeart}
-        />
-        {/* <button name={row}>
-            <FontAwesomeIcon
-              style={{
-                color: 'black',
-                fontSize: '20px',
-              }}
-              icon={faXmark}
-            />
-            X
-          </button> */}
+        {data ? (
+          <LikeButton
+            songName={result}
+            likedSong={data.findLikedSongs.includes(result)}
+          />
+        ) : null}
       </ListGroup.Item>
     )
   }
